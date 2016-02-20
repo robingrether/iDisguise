a179001193f943341582457d29b8f604175d5e74
Robin Grether <robingrether@web.de>
2016-02-20 23:35:47 +0100

    Add new methods to disguise/undisguise offline players.

--- iDisguise/src/de/robingrether/idisguise/api/DisguiseAPI.java
+++ iDisguise/src/de/robingrether/idisguise/api/DisguiseAPI.java
@@ -1,5 +1,6 @@
 package de.robingrether.idisguise.api;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import de.robingrether.idisguise.disguise.Disguise;
@@ -18,22 +19,70 @@ public interface DisguiseAPI {
 	/**
 	 * Disguises a player.
 	 * 
+	 * @deprecated replaced by <code>disguise(OfflinePlayer, Disguise)</code>
 	 * @since 3.0.1
 	 * @param player the player to disguise
 	 * @param disguise the disguise
 	 */
+	@Deprecated
 	public void disguiseToAll(Player player, Disguise disguise);
 	
 	/**
 	 * Undisguises a player.
 	 * 
+	 * @deprecated replaced by <code>undisguise(OfflinePlayer)</code>
 	 * @since 2.1.3
 	 * @param player the player to undisguise
 	 */
+	@Deprecated
 	public void undisguiseToAll(Player player);
 	
 	/**
-	 * Undisguises all players.
+	 * Disguise a player (may be offline).<br>
+	 * Calling <code>disguise(player, disguise)</code> is equivalent to calling <code>disguise(player, disguise, true)</code>.<br>
+	 * This method always fires a {@linkplain DisguiseEvent} which may be cancelled by other plugins.
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player) to disguise
+	 * @param disguise the disguise
+	 * @return <code>false</code>, in case the {@linkplain DisguiseEvent} has been cancelled
+	 */
+	public boolean disguise(OfflinePlayer player, Disguise disguise);
+	
+	/**
+	 * Disguise a player (may be offline).
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player) to disguise
+	 * @param disguise the disguise
+	 * @param fireEvent whether a {@linkplain DisguiseEvent} should be fired (event may be cancelled by other plugins)
+	 * @return <code>false</code>, in case the {@linkplain DisguiseEvent} has been cancelled
+	 */
+	public boolean disguise(OfflinePlayer player, Disguise disguise, boolean fireEvent);
+	
+	/**
+	 * Undisguise a player (may be offline).<br>
+	 * Calling <code>undisguise(player)</code> is equivalent to calling <code>undisguise(player, true)</code>.<br>
+	 * This method always fires an {@linkplain UndisguiseEvent} which may be cancelled by other plugins.
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player) to undisguise
+	 * @return <code>false</code>, in case the {@linkplain UndisguiseEvent} has been cancelled
+	 */
+	public boolean undisguise(OfflinePlayer player);
+	
+	/**
+	 * Undisguise a player (may be offline).
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player) to undisguise
+	 * @param fireEvent whether an {@linkplain UndisguiseEvent} should be fired (event may be cancelled by other plugins)
+	 * @return <code>false</code>, in case the {@linkplain UndisguiseEvent} has been cancelled
+	 */
+	public boolean undisguise(OfflinePlayer player, boolean fireEvent);
+	
+	/**
+	 * Undisguise everyone.
 	 * 
 	 * @since 2.1.3
 	 */
@@ -42,20 +91,42 @@ public interface DisguiseAPI {
 	/**
 	 * Checks whether a player is disguised.
 	 * 
+	 * @deprecated replaced by <code>isDisguised(OfflinePlayer)</code>
 	 * @since 2.1.3
 	 * @param player the player to check
 	 * @return true if disguised, false if not
 	 */
+	@Deprecated
 	public boolean isDisguised(Player player);
+	
+	/**
+	 * Check whether a player is disguised.
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player) to check
+	 * @return <code>true</code>, if the player is disguised
+	 */
+	public boolean isDisguised(OfflinePlayer player);
 	
 	/**
 	 * Gets a copy of a player's disguise.
 	 * 
+	 * @deprecated replaced by <code>getDisguise(OfflinePlayer)</code>
 	 * @since 2.1.3
 	 * @param player the player
 	 * @return the disguise or null if not disguised
 	 */
+	@Deprecated
 	public Disguise getDisguise(Player player);
+	
+	/**
+	 * Get a copy of a player's current disguise.
+	 * 
+	 * @since 5.1.1
+	 * @param player the player (or offline player)
+	 * @return a <strong>copy</strong> of the given player's disguise
+	 */
+	public Disguise getDisguise(OfflinePlayer player);
 	
 	/**
 	 * Counts the amount of online players who are disguised.
