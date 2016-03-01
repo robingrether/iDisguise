c83ae1d2fed3bd6c6dfb92cef35ba1320082b16e
Robin Grether <robingrether@web.de>
2016-03-01 22:29:15 +0100

    Add 1.9 implementation.

--- iDisguise/src/de/robingrether/idisguise/management/VersionHelper.java
+++ iDisguise/src/de/robingrether/idisguise/management/VersionHelper.java
@@ -6,7 +6,7 @@ import de.robingrether.util.StringUtil;
 
 public class VersionHelper {
 	
-	private static final String[] versions = {"v1_5_R2", "v1_5_R3", "v1_6_R1", "v1_6_R2", "v1_6_R3", "v1_7_R1", "v1_7_R2", "v1_7_R3", "v1_7_R4", "v1_8_R1", "v1_8_R2", "v1_8_R3"};
+	private static final String[] versions = {"v1_5_R2", "v1_5_R3", "v1_6_R1", "v1_6_R2", "v1_6_R3", "v1_7_R1", "v1_7_R2", "v1_7_R3", "v1_7_R4", "v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1"};
 	private static boolean initialized = false;
 	private static String versionCode, orgBukkitCraftbukkit = "org.bukkit.craftbukkit", netMinecraftServer = "net.minecraft.server", orgBukkitCraftbukkitVersioned, netMinecraftServerVersioned;
 	
@@ -52,6 +52,13 @@ public class VersionHelper {
 		orgBukkitCraftbukkitVersioned = orgBukkitCraftbukkit + "." + versionCode;
 		netMinecraftServerVersioned = netMinecraftServer + "." + versionCode;
 		switch(versionCode) {
+			case "v1_9_R1":
+				ChannelRegister.instance = new de.robingrether.idisguise.management.impl.v1_9_R1.ChannelRegisterImpl();
+				DisguiseManager.instance = new de.robingrether.idisguise.management.impl.v1_9_R1.DisguiseManagerImpl();
+				GhostFactory.instance = new de.robingrether.idisguise.management.impl.v1_8_R3.GhostFactoryImpl();
+				PacketHelper.instance = new de.robingrether.idisguise.management.impl.v1_9_R1.PacketHelperImpl();
+				PlayerHelper.instance = new de.robingrether.idisguise.management.impl.v1_9_R1.PlayerHelperImpl();
+				break;
 			case "v1_8_R3":
 				ChannelRegister.instance = new de.robingrether.idisguise.management.impl.v1_8_R3.ChannelRegisterImpl();
 				DisguiseManager.instance = new de.robingrether.idisguise.management.impl.v1_8_R3.DisguiseManagerImpl();
