cf46580372a15f9097cf4c1af3fbcc392b511fd1
Robin Grether <privat@robingrether.de>
2017-05-28 22:34:04 +0200

    Rework player helper.

--- idisguise-core/src/main/java/de/robingrether/idisguise/management/PlayerHelper.java
+++ idisguise-core/src/main/java/de/robingrether/idisguise/management/PlayerHelper.java
@@ -1,23 +1,12 @@
 package de.robingrether.idisguise.management;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.logging.Level;
 
 import org.bukkit.entity.Player;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
-import de.robingrether.idisguise.iDisguise;
-
-public class PlayerHelper {
+public abstract class PlayerHelper {
 	
 	private static PlayerHelper instance;
 	
@@ -29,7 +18,6 @@ public class PlayerHelper {
 		PlayerHelper.instance = instance;
 	}
 	
-	private final String API_URL = "https://api.mojang.com/user/profiles/";
 	private Map<Integer, Player> players;
 	
 	public PlayerHelper() {
@@ -51,54 +39,20 @@ public class PlayerHelper {
 		return players.get(entityId);
 	}
 	
-	public String getCaseCorrectedName(String name) {
-		return name;
-	}
+	public abstract String getCaseCorrectedName(String name);
 	
-	public UUID getUniqueId(String name) {
-		return null;
-	}
+	public abstract UUID getUniqueId(String name);
 	
-	public String getName(UUID uniqueId) {
-		BufferedReader reader = null;
-		try {
-			URL url = new URL(API_URL + uniqueId.toString().replace("-", "") + "/names");
-			URLConnection connection = url.openConnection();
-			connection.addRequestProperty("User-Agent", iDisguise.getInstance().getFullName().replace(' ', '/') + " (by RobinGrether)");
-			connection.setDoOutput(true);
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String response = reader.readLine();
-			JSONArray array = (JSONArray)JSONValue.parse(response);
-			JSONObject object = (JSONObject)array.get(0);
-			return (String)object.get("name");
-		} catch(IOException e) {
-			if(VersionHelper.debug()) {
-				iDisguise.getInstance().getLogger().log(Level.SEVERE, "Cannot convert the stored disguise data.", e);
-			}
-		} catch(NullPointerException e) {
-		} finally {
-			if(reader != null) {
-				try {
-					reader.close();
-				} catch(IOException e) {
-				}
-			}
-		}
-		return null;
-	}
+	public abstract String getName(UUID uniqueId);
 	
-	public Object getGameProfile(UUID uniqueId, String skinName, String displayName) {
-		return null;
-	}
+	public abstract Object getGameProfile(UUID uniqueId, String skinName, String displayName);
 	
-	public void loadGameProfileAsynchronously(String skinName) {
-	}
+	public abstract void loadGameProfileAsynchronously(String skinName);
 	
-	public boolean isGameProfileLoaded(String skinName) {
-		return true;
-	}
+	public abstract void loadGameProfileAsynchronously(UUID uniqueId);
 	
-	public void waitForGameProfile(String skinName) {
-	}
+	public abstract boolean isGameProfileLoaded(String skinName);
+	
+	public abstract void waitForGameProfile(String skinName);
 	
 }
--- idisguise-impl-v1_8_R3/src/main/java/de/robingrether/idisguise/management/player/PlayerHelperUID18.java
+++ idisguise-impl-v1_8_R3/src/main/java/de/robingrether/idisguise/management/player/PlayerHelperUID18.java
@@ -1,5 +1,10 @@
 package de.robingrether.idisguise.management.player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
@@ -7,91 +12,207 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 import com.mojang.authlib.GameProfile;
-
-import static de.robingrether.idisguise.management.Reflection.*;
+import com.mojang.authlib.properties.Property;
 
 import de.robingrether.idisguise.iDisguise;
 import de.robingrether.idisguise.management.PlayerHelper;
-import de.robingrether.idisguise.management.VersionHelper;
 
 public class PlayerHelperUID18 extends PlayerHelper {
 	
-	// only lower case names
-	private final Map<String, GameProfile> gameProfiles = new ConcurrentHashMap<String, GameProfile>();
-	private final Map<String, Object> currentlyLoading =  new ConcurrentHashMap<String, Object>();
+	public static final String API_NAME_URL = "https://api.mojang.com/users/profiles/minecraft/";
+	public static final String API_NAME_ID = "id";
+	
+	public static final String API_UID_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
+	public static final String API_UID_ID = "id";
+	public static final String API_UID_NAME = "name";
+	public static final String API_UID_PROPERTIES = "properties";
+	public static final String API_UID_SIGNATURE = "signature";
+	public static final String API_UID_VALUE = "value";
+	
+	private final Map<String, GameProfile> profilesByName = new ConcurrentHashMap<String, GameProfile>();
+	private final Map<UUID, GameProfile> profilesById = new ConcurrentHashMap<UUID, GameProfile>();
+	
+	private final Map<String, Object> currentlyLoadingByName = new ConcurrentHashMap<String, Object>();
+	private final Map<UUID, Object> currentlyLoadingById = new ConcurrentHashMap<UUID, Object>();
+	
+	private void loadGameProfile(String name) {
+		name = name.toLowerCase(Locale.ENGLISH);
+		if(currentlyLoadingByName.containsKey(name)) {
+			synchronized(currentlyLoadingByName.get(name)) {
+				try {
+					currentlyLoadingByName.get(name).wait(10000L);
+				} catch (InterruptedException e) {
+				}
+				return;
+			}
+		}
+		currentlyLoadingByName.put(name, new Object());
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(API_NAME_URL + name);
+			URLConnection connection = url.openConnection();
+			connection.addRequestProperty("User-Agent", iDisguise.getInstance().getFullName());
+			connection.setDoOutput(true);
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String response = reader.readLine();
+			JSONObject object = (JSONObject)JSONValue.parse(response);
+			UUID uniqueId = UUID.fromString(((String)object.get(API_NAME_ID)).replaceFirst("([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})", "$1-$2-$3-$4-$5"));
+			loadGameProfile(uniqueId);
+			synchronized(currentlyLoadingByName.get(name)) {
+				currentlyLoadingByName.remove(name).notifyAll();
+			}
+		} catch(Exception e) {
+			iDisguise.getInstance().getLogger().log(Level.SEVERE, "Cannot retrieve the required profile information.", e);
+		} finally {
+			if(reader != null) {
+				try {
+					reader.close();
+				} catch(IOException e) {
+				}
+			}
+		}
+	}
+	
+	private void loadGameProfile(UUID uniqueId) {
+		if(currentlyLoadingById.containsKey(uniqueId)) {
+			synchronized(currentlyLoadingById.get(uniqueId)) {
+				try {
+					currentlyLoadingById.get(uniqueId).wait(10000L);
+				} catch (InterruptedException e) {
+				}
+				return;
+			}
+		}
+		currentlyLoadingById.put(uniqueId, new Object());
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(API_UID_URL + uniqueId.toString().replace("-", "") + "?unsigned=false");
+			URLConnection connection = url.openConnection();
+			connection.addRequestProperty("User-Agent", iDisguise.getInstance().getFullName());
+			connection.setDoOutput(true);
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String response = reader.readLine();
+			JSONObject object = (JSONObject)JSONValue.parse(response);
+			String name = (String)object.get(API_UID_NAME);
+			GameProfile profile = new GameProfile(uniqueId, name);
+			JSONArray array = (JSONArray)object.get(API_UID_PROPERTIES);
+			for(Object obj : array) {
+				JSONObject property = (JSONObject)obj;
+				String propertyName = (String)property.get(API_UID_NAME);
+				profile.getProperties().put(propertyName, new Property(propertyName, (String)property.get(API_UID_VALUE), (String)property.get(API_UID_SIGNATURE)));
+			}
+			profilesById.put(uniqueId, profile);
+			profilesByName.put(name.toLowerCase(Locale.ENGLISH), profile);
+			synchronized(currentlyLoadingById.get(uniqueId)) {
+				currentlyLoadingById.remove(uniqueId).notifyAll();
+			}
+		} catch(Exception e) {
+			iDisguise.getInstance().getLogger().log(Level.SEVERE, "Cannot retrieve the required profile information.", e);
+		} finally {
+			if(reader != null) {
+				try {
+					reader.close();
+				} catch(IOException e) {
+				}
+			}
+		}
+	}
 	
 	public String getCaseCorrectedName(String name) {
-		return Bukkit.getOfflinePlayer(name).getName();
+		name = name.toLowerCase(Locale.ENGLISH);
+		if(profilesByName.containsKey(name)) {
+			return profilesByName.get(name).getName();
+		} else {
+			loadGameProfile(name);
+			if(profilesByName.containsKey(name)) {
+				return profilesByName.get(name).getName();
+			} else {
+				return name;
+			}
+		}
 	}
 	
 	public UUID getUniqueId(String name) {
-		return Bukkit.getOfflinePlayer(name).getUniqueId();
+		name = name.toLowerCase(Locale.ENGLISH);
+		if(profilesByName.containsKey(name)) {
+			return profilesByName.get(name).getId();
+		} else {
+			loadGameProfile(name);
+			if(profilesByName.containsKey(name)) {
+				return profilesByName.get(name).getId();
+			} else {
+				return UUID.randomUUID();
+			}
+		}
 	}
 	
 	public String getName(UUID uniqueId) {
-		return Bukkit.getOfflinePlayer(uniqueId).getName();
+		if(profilesById.containsKey(uniqueId)) {
+			return profilesById.get(uniqueId).getName();
+		} else {
+			loadGameProfile(uniqueId);
+			if(profilesById.containsKey(uniqueId)) {
+				return profilesById.get(uniqueId).getName();
+			} else {
+				return null;
+			}
+		}
 	}
 	
 	public GameProfile getGameProfile(UUID uniqueId, String skinName, String displayName) {
-		GameProfile localGameProfile = new GameProfile(uniqueId, displayName.length() <= 16 ? displayName : skinName);
-		if(gameProfiles.containsKey(skinName.toLowerCase(Locale.ENGLISH))) {
-			localGameProfile.getProperties().putAll(gameProfiles.get(skinName.toLowerCase(Locale.ENGLISH)).getProperties());
+		GameProfile localProfile = new GameProfile(uniqueId, displayName.length() <= 16 ? displayName : skinName);
+		if(profilesByName.containsKey(skinName.toLowerCase(Locale.ENGLISH))) {
+			localProfile.getProperties().putAll(profilesByName.get(skinName.toLowerCase(Locale.ENGLISH)).getProperties());
 		}
-		return localGameProfile;
+		return localProfile;
 	}
 	
-	public synchronized void loadGameProfileAsynchronously(final String skinName) {
-		if(gameProfiles.containsKey(skinName.toLowerCase(Locale.ENGLISH)) || currentlyLoading.containsKey(skinName.toLowerCase(Locale.ENGLISH))) {
+	public void loadGameProfileAsynchronously(String skinName) {
+		final String name = skinName.toLowerCase(Locale.ENGLISH);
+		if(profilesByName.containsKey(name) || currentlyLoadingByName.containsKey(name)) {
 			return;
 		}
-		currentlyLoading.put(skinName.toLowerCase(Locale.ENGLISH), new Object());
 		Bukkit.getScheduler().runTaskAsynchronously(iDisguise.getInstance(), new Runnable() {
 			
 			public void run() {
-				GameProfile gameProfile = loadGameProfile(skinName);
-				if(gameProfile != null) {
-					gameProfiles.put(skinName.toLowerCase(Locale.ENGLISH), gameProfile);
-				}
-				synchronized(currentlyLoading.get(skinName.toLowerCase(Locale.ENGLISH))) {
-					currentlyLoading.remove(skinName.toLowerCase(Locale.ENGLISH)).notifyAll();
-				}
+				loadGameProfile(name);
 			}
 			
 		});
 	}
 	
-	public boolean isGameProfileLoaded(String skinName) {
-		return gameProfiles.containsKey(skinName.toLowerCase(Locale.ENGLISH));
+	public void loadGameProfileAsynchronously(final UUID uniqueId) {
+		if(profilesById.containsKey(uniqueId) || currentlyLoadingById.containsKey(uniqueId)) {
+			return;
+		}
+		Bukkit.getScheduler().runTaskAsynchronously(iDisguise.getInstance(), new Runnable() {
+			
+			public void run() {
+				loadGameProfile(uniqueId);
+			}
+			
+		});
 	}
 	
-	public void waitForGameProfile(String skinName) {
-		if(currentlyLoading.containsKey(skinName.toLowerCase(Locale.ENGLISH))) {
-			try {
-				synchronized(currentlyLoading.get(skinName.toLowerCase(Locale.ENGLISH))) {
-					currentlyLoading.get(skinName.toLowerCase(Locale.ENGLISH)).wait(10000L);
+	public boolean isGameProfileLoaded(String name) {
+		return profilesByName.containsKey(name.toLowerCase(Locale.ENGLISH));
+	}
+	
+	public void waitForGameProfile(String name) {
+		name = name.toLowerCase(Locale.ENGLISH);
+		if(currentlyLoadingByName.containsKey(name)) {
+			synchronized(currentlyLoadingByName.get(name)) {
+				try {
+					currentlyLoadingByName.get(name).wait(10000L);
+				} catch (InterruptedException e) {
 				}
-			} catch(InterruptedException e) {
 			}
 		}
-	}
-	
-	private GameProfile loadGameProfile(String skinName) {
-		try {
-			OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(skinName);
-			GameProfile gameProfile = (GameProfile)(offlinePlayer.isOnline() ? CraftPlayer_getProfile.invoke(offlinePlayer) : CraftOfflinePlayer_getProfile.invoke(offlinePlayer));
-			if(gameProfile.getProperties().isEmpty()) {
-				MinecraftSessionService_fillProfileProperties.invoke(MinecraftServer_getSessionService.invoke(MinecraftServer_getServer.invoke(null)), gameProfile, true);
-			}
-			return gameProfile;
-		} catch(Exception e) {
-			if(VersionHelper.debug()) {
-				iDisguise.getInstance().getLogger().log(Level.SEVERE, "Cannot retrieve the required profile information.", e);
-			}
-		}
-		return null;
 	}
 	
 }
