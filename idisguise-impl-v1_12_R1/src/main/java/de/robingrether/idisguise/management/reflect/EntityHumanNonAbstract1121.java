83d90b407c38655127016e9c41c4eefc228d4395
Robin Grether <privat@robingrether.de>
2017-10-19 21:41:28 +0200

    Implementation for 1.12, so we can do initial testing.

---
+++ idisguise-impl-v1_12_R1/src/main/java/de/robingrether/idisguise/management/reflect/EntityHumanNonAbstract1121.java
@@ -0,0 +1,18 @@
+package de.robingrether.idisguise.management.reflect;
+
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.server.v1_12_R1.EntityHuman;
+import net.minecraft.server.v1_12_R1.World;
+
+public class EntityHumanNonAbstract1121 extends EntityHuman {
+	
+	public EntityHumanNonAbstract1121(World world, GameProfile gameProfile) {
+		super(world, gameProfile);
+	}
+	
+	public boolean isSpectator() { return false; }
+	
+	public boolean z() { return false; }
+	
+}
