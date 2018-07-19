f02a17dc929839ce9e3134675157cc7a0fe127ca
Robin Grether <privat@robingrether.de>
2018-07-19 21:08:16 +0200

    Move reflection info back to core project.

--- idisguise-core/src/main/java/de/robingrether/idisguise/management/VersionHelper.java
+++ idisguise-core/src/main/java/de/robingrether/idisguise/management/VersionHelper.java
@@ -1,19 +1,21 @@
 package de.robingrether.idisguise.management;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 
 import de.robingrether.idisguise.management.channel.ChannelInjector;
 import de.robingrether.idisguise.management.util.EntityIdList;
-import de.robingrether.util.StringUtil;
 
 public class VersionHelper {
 	
-	public static final String[] VERSIONS = {"v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1", "v1_9_R2", "v1_10_R1", "v1_11_R1", "v1_12_R1"};
-	public static final String EARLIEST = VERSIONS[0];
+	public static final List<String> VERSIONS = Arrays.asList("v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1", "v1_9_R2", "v1_10_R1", "v1_11_R1", "v1_12_R1");
+	public static final String EARLIEST = VERSIONS.get(0);
 	
 	private static boolean initialized = false;
 	private static String versionCode, orgBukkitCraftbukkit = "org.bukkit.craftbukkit", netMinecraftServer = "net.minecraft.server", orgBukkitCraftbukkitVersioned, netMinecraftServerVersioned;
-	private static boolean debug, require1_9, require1_10, require1_11, require1_12;
+	private static boolean debug, require1_9, require1_10, require1_11, require1_12, require1_13;
 	
 	public static String getVersionCode() {
 		return versionCode;
@@ -32,7 +34,7 @@ public class VersionHelper {
 	}
 	
 	public static boolean requireVersion(String requiredVersion) {
-		if(!StringUtil.equals(requiredVersion, VERSIONS)) {
+		if(!VERSIONS.contains(requiredVersion)) {
 			return false;
 		}
 		for(String version : VERSIONS) {
@@ -43,6 +45,10 @@ public class VersionHelper {
 			}
 		}
 		return false;
+	}
+	
+	public static boolean require1_13() {
+		return require1_13;
 	}
 	
 	public static boolean require1_12() {
@@ -67,38 +73,30 @@ public class VersionHelper {
 		}
 		try {
 			versionCode = Bukkit.getServer().getClass().getPackage().getName().substring(orgBukkitCraftbukkit.length() + 1);
+			if(!VERSIONS.contains(versionCode)) return false;
+			
 			orgBukkitCraftbukkitVersioned = orgBukkitCraftbukkit + "." + versionCode;
 			netMinecraftServerVersioned = netMinecraftServer + "." + versionCode;
 			VersionHelper.debug = debug;
+			
 			require1_9 = requireVersion("v1_9_R1");
 			require1_10 = requireVersion("v1_10_R1");
 			require1_11 = requireVersion("v1_11_R1");
 			require1_12 = requireVersion("v1_12_R1");
+			require1_13 = false; // TODO
+			
 			Reflection.load("reflection/common.txt", netMinecraftServerVersioned, orgBukkitCraftbukkitVersioned);
-			switch(versionCode) {
-				case "v1_8_R1":
-				case "v1_8_R2":
-				case "v1_8_R3":
-					Reflection.load("reflection/" + versionCode + ".txt", netMinecraftServerVersioned, orgBukkitCraftbukkitVersioned);
-					Sounds.init("sounds/18.yml");
-					break;
-				case "v1_9_R1":
-				case "v1_9_R2":
-				case "v1_10_R1":
-				case "v1_11_R1":
-				case "v1_12_R1":
-					Reflection.load("reflection/" + versionCode + ".txt", netMinecraftServerVersioned, orgBukkitCraftbukkitVersioned);
-					Sounds.init("sounds/112.yml");
-					break;
-				default:
-					return false;
-			}
+			Reflection.load("reflection/" + versionCode + ".txt", netMinecraftServerVersioned, orgBukkitCraftbukkitVersioned);
 			Reflection.load("reflection/common2.txt", netMinecraftServerVersioned, orgBukkitCraftbukkitVersioned);
 			ChannelInjector.init();
 			EntityIdList.init();
 			ProfileHelper.setInstance((ProfileHelper)Class.forName("de.robingrether.idisguise.management.profile.ProfileHelperUID").newInstance());
 			Reflection.EntityHumanNonAbstract = Class.forName("de.robingrether.idisguise.management.reflection.EntityHumanNonAbstract" + versionCode.replaceAll("[^0-9]*", ""));
 			Reflection.EntityHumanNonAbstract_new = Reflection.EntityHumanNonAbstract.getConstructor(Reflection.World, Reflection.GameProfile);
+			
+			if(require1_9) Sounds.init("sounds/112.yml");
+			else Sounds.init("sounds/18.yml");
+			
 			initialized = true;
 			return true;
 		} catch(Exception e) {
