c9cc7fdc8fd683d41906fa74a3f52fc23caffc0a
Robin Grether <privat@robingrether.de>
2018-11-24 11:14:45 +0100

    Fix player disguise in 1.8.

--- idisguise-impl-v1_8_R3/src/main/java/de/robingrether/idisguise/management/reflection/EntityHumanNonAbstract183.java
+++ idisguise-impl-v1_8_R3/src/main/java/de/robingrether/idisguise/management/reflection/EntityHumanNonAbstract183.java
@@ -10,7 +10,7 @@ public class EntityHumanNonAbstract183 extends EntityHuman {
 	public EntityHumanNonAbstract183(World world, GameProfile gameProfile) {
 		super(world, gameProfile);
 		
-		getDataWatcher().a(10, (byte)0x7f);
+		getDataWatcher().watch(10, (byte)0x7f);
 	}
 	
 	public boolean isSpectator() { return false; }
